ccac03f2f0fdae8603932ea58a0ab8d50355becf
Kate <ms.katesy@gmail.com>
2018-09-18 13:56:15 +1000

    made contact page and styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,17 +10,27 @@ export default function About (){
         <p>{"const kate = {" }</p>
         <p>{tab}{"from: 'San Francisco'," }</p>
         <p>{tab}{"livesIn: 'Melbourne'," }</p>
-        <p>{tab}{"favoriteProgrammingLanguage: 'Javascript'," }</p>
+        <p>{tab}{"codingSince: '18th June 2018'," }</p>
+        <p>{tab}{"favouriteLanguage: 'Javascript'," }</p>
+        <p>{tab}{"currentlyLearning: 'React'," }</p>
+        <p>{tab}{"previousLives: [" }</p>
+        <p>{tab}{tab}{"'bank teller','accountant',"}</p>
+        <p>{tab}{tab}{"'receptionist', 'admissions'"}</p>        
+        <p>{tab}{tab}{"'sales reportings, quality assurance'"}</p>
+        <p>{tab}{"]" }</p>
         <p>{tab}{"values: [" }</p>
         <p>{tab}{tab}{"'creativity','learning', 'family',"}</p>
         <p>{tab}{tab}{"'spreading happiness'"}</p>
         <p>{tab}{"]" }</p>
         <p>{tab}{"hobbies: [" }</p>
         <p>{tab}{tab}{"'ukulele','drone photography',"}</p>
-        <p>{tab}{tab}{"'walking/hiking', 'board games'"}</p>
+        <p>{tab}{tab}{"'walking/hiking', 'board games',"}</p>
+        <p>{tab}{tab}{"'studying Japanese'"}</p>
         <p>{tab}{"]" }</p>
+        <p>{tab}{"favouriteBook: 'Dune by Frank Herbert'," }</p>
+        <p>{tab}{"MBTI:"} <a href="https://www.16personalities.com/isfp-personality" rel="noopener noreferrer" target="_blank">'ISFP'</a></p>
         <p>{tab}{"lovesCats: true," }</p>
-        <p>{tab}{"volunteersWith:"} <a href="https://www.cheltenhamcatrescue.org.au/" target="_blank">'cheltenham cat rescue'</a></p>
+        <p>{tab}{"volunteersWith:"} <a rel="noopener noreferrer" href="https://www.cheltenhamcatrescue.org.au/" target="_blank">'cheltenham cat rescue'</a></p>
         <p>{"}"}</p>
       </div>
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './Contact.css'
 
 export default function Contact (){
 
   return (
-    <div>
-      <h1>say hi!</h1>
+    <div className="contact-wrapper">
+      <div className="info">
+        <img src="https://i.imgur.com/dp3up0t.jpg" alt=""/>
+        <h2>Kate Sy</h2>
+        <h3>Full-Stack Web Developer</h3>
+      </div>
+      <div className="contact-links">
+        <p><i className="fab fa-github"></i> <a target="_blank" href="https://github.com/katesyberspace">katesyberspace</a></p>
+        <p><i className="fab fa-linkedin"></i> <a target="_blank" href="https://www.linkedin.com/in/katesy/">Kate Sy</a></p>
+        <p><i className="fas fa-envelope"></i> <a target="_blank" href="mailto:ms.katesy@gmail.com">ms.katesy@gmail.com</a></p>
+        <p><i className="fab fa-instagram"></i> <a target="_blank" href="https://www.instagram.com/katesyberspace">katesyberspace</a></p>
+
+      </div>
     </div>
   )
 }
